0982199550e7b228c265553b2cd6a70ed0060af2
sai770 <sai7127898@gmail.com>
2022-06-08 22:23:37 +0530

    conflict resolved by dev2

--- hello.java
+++ hello.java
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 class hello{ 2
 
-	public static void from on 
-=======
+	public static void from
 class hello{ 3
 
 	public static void from 
 >>>>>>> 7bc3261a389eebc1f0f1090d93df6923994ebb64
-=======
-class hello{ 3
 
-	public static void from 
->>>>>>> 7bc3261a389eebc1f0f1090d93df6923994ebb64
-			system.out.print
-to
        hello modified
 } 5
