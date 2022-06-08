767b73f7179d62e8cecaf1d982837b5e01365dec
sai770 <sai7127898@gmail.com>
2022-06-08 22:04:06 +0530

    dev2

--- hello.java
+++ hello.java
@@ -1,7 +1,13 @@
-class hello{
+<<<<<<< HEAD
+class hello{ 2
 
-	public static void 
+	public static void from on 
+=======
+class hello{ 3
+
+	public static void from 
+>>>>>>> 7bc3261a389eebc1f0f1090d93df6923994ebb64
 			system.out.print
-
+to
        hello modified
-}
+} 5
